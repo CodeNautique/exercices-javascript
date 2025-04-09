788400abf36cb225e062c3c3766b8c33866c3034
TomCode <ltoly@outlook.com>
2025-04-09 12:16:38 +0200

    Initial commit

---
+++ 01-declaration-variable/README.md
@@ -0,0 +1,16 @@
+## Déclaration de variables
+
+## Exercice 1 : Informations personnelles
+
+Déclare trois variables : `prenom`, `age`, `ville`.  
+Puis affiche-les dans une phrase comme :
+
+> "Je m'appelle Marie, j'ai 28 ans et j'habite à Lyon."
+
+## Résultat attendu (console)
+Je m'appelle John, j'ai 25 et j'habite à Paris.
+
+## Ce que j'ai appris :
+- Déclarer des variables avec `const` et `let`
+- Utiliser une **template string** avec les backticks (`` ` ``)
+- Afficher une chaîne formatée dans la console
---
+++ 01-declaration-variable/exercice-1.js
@@ -0,0 +1,21 @@
+/* Niveau débutant — Déclarations simples
+✅ Exercice 1 : Informations personnelles
+Déclare 3 variables :
+
+prenom (une chaîne de caractères avec ton prénom)
+
+age (un nombre)
+
+ville (une chaîne avec ta ville)
+
+Affiche les trois en une seule ligne dans la console, avec une phrase comme :
+
+"Je m'appelle Marie, j'ai 28 ans et j'habite à Lyon." */
+
+
+const prenom = "John";
+let age = 25;
+let ville = "Paris";
+let message = `Je m'appelle ${prenom}, j'ai ${age} et j'habite à ${ville}.`;
+
+console.log(message);
